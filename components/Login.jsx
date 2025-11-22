eeb43495ebed35dbd189f7a3ccf203db8a9ffb94
kavi-gomez <thuspura@gmail.com>
2025-11-22 17:19:36 +0530

    Refactor to single-screen login and simplify layout
    Removed the tab-based navigation and related components, consolidating the app to a single login screen. Updated the layout to load custom fonts and display only the Login component. Added new assets and a Login component, and renamed constants/theme.ts to constants/Colors.ts with additional color constants. Removed unused hooks and UI components related to the previous tabbed structure.

---
+++ components/Login.jsx
@@ -0,0 +1,70 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import { Colors } from "../constants/Colors";
+
+export default function Login() {
+  return (
+    <View>
+      <Image
+        source={require("../assets/images/login.png")}
+        style={{ width: "100%", height: 520 }}
+      />
+
+      <View style={styles.container}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontFamily: "outfit-bold",
+            textAlign: "center",
+            marginTop: 10,
+          }}
+        >
+          AI Travel Planner
+        </Text>
+
+        <Text
+          style={{
+            fontSize: 17,
+            fontFamily: "outfit",
+            textAlign: "center",
+            color: Colors.GRAY,
+            marginTop: 20,
+          }}
+        >
+          Discover your next adventure effortlessly. Personalized itinanries at
+          your fingertips. Travel smarter with AI driven insights
+        </Text>
+
+        <View style={styles.button}>
+          <Text
+            style={{
+              color: Colors.WHITE,
+              textAlign: "center",
+              fontFamily: "outfit",
+              fontSize: 17,
+            }}
+          >
+            Sign in with Google
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    marginTop: -20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    height: "100%",
+    padding: 25,
+  },
+  button: {
+    backgroundColor: Colors.PRIMARY,
+    padding: 15,
+    borderRadius: 99,
+    marginTop: "20%",
+  },
+});
