c3ba6eafae2698bd551cf9da7e3208e9b45b035c
kavi-gomez <thuspura@gmail.com>
2025-11-23 15:32:00 +0530

    Add My Trips screen UI and StartNewTripCard component
    Implemented the My Trips screen with a header and conditional rendering for when there are no trips. Added the StartNewTripCard component to prompt users to start a new trip when no trips are present.

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,5 +1,41 @@
-import { View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { useState } from "react";
+import { Text, View } from "react-native";
+import StartNewTripCard from "../../components/MyTrips/StartNewTripCard";
+import { Colors } from "../../constants/Colors";
 
 export default function MyTrip() {
-  return <View></View>;
+  const [useTrips, setUseTrips] = useState([]);
+
+  return (
+    <View
+      style={{
+        padding: 25,
+        paddingTop: 55,
+        backgroundColor: Colors.WHITE,
+        height: "100%",
+      }}
+    >
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignContent: "center",
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "outfit-bold",
+            fontSize: 35,
+          }}
+        >
+          My Trips
+        </Text>
+        <Ionicons name="add-circle" size={50} color="black" />
+      </View>
+
+      {useTrips.length === 0 ? <StartNewTripCard /> : null}
+    </View>
+  );
 }
